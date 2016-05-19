a9ab88d2a0d50aa79c05ba760604b32ec11cbfe1
akigupta131 <akigupta131@gmail.com>
2016-05-19 18:40:39 +0530

    Mark As Complete feature works now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519061249) do
+ActiveRecord::Schema.define(version: 20160519130347) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160519061249) do
     t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "completed_at"
   end
 
   add_index "tasks", ["list_id"], name: "index_tasks_on_list_id"
