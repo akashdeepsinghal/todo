85256fee5831d0a00c8cc3c2ce6726a479dec360
akigupta131 <akigupta131@gmail.com>
2016-05-19 18:01:41 +0530

    tasks inside list being manipulated | Create and Destroy ops working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519054022) do
+ActiveRecord::Schema.define(version: 20160519061249) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
@@ -21,11 +21,13 @@ ActiveRecord::Schema.define(version: 20160519054022) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "content"
+    t.integer  "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  add_index "tasks", ["list_id"], name: "index_tasks_on_list_id"
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
