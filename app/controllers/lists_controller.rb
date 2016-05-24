88ff23cbae5ca7b8adbf68498522e388b47f6f0a
akigupta131 <akigupta131@gmail.com>
2016-05-24 14:32:05 +0530

    CREATE query through Postman

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /lists
   # GET /lists.json
